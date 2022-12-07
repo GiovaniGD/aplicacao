0b5b5ca5d81f3529084819210c1ddba0992f52c2
GiovaniGD <100315032+GiovaniGD@users.noreply.github.com>
2022-12-07 14:51:33 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+function limparInformacoes(){
+    document.getElementById('rua').value=("");
+    document.getElementById('bairro').value=("");
+    document.getElementById('cidade').value=("");
+    document.getElementById('uf').value=("");
+}
+
+function meu_callback(conteudo){
+if(!("erro" in conteudo)){
+    document.getElementById('rua').value=(conteudo.logradouro);
+    document.getElementById('bairro').value=(conteudo.bairro);
+    document.getElementById('cidade').value=(conteudo.localidade);
+    document.getElementById('uf').value=(conteudo.uf);
+}
+else{
+    limparInformacoes();
+    return;
+}
+}
+
+function pesquisarCep(valor){
+var cep = valor.replace(/\D/g, '');
+
+if (cep != ""){
+    var validarCep = /^[0-9]{8}$/;
+
+    if(validarCep.test(cep)){
+        document.getElementById('rua').value="...";
+        document.getElementById('bairro').value="...";
+        document.getElementById('cidade').value="...";
+        document.getElementById('uf').value="...";
+
+        var script = document.createElement('script');
+
+        script.src = `https://viacep.com.br/ws/${cep}/json/?callback=meu_callback`;
+
+        document.body.appendChild(script);
+    }
+    else{
+        limparInformacoes();
+        return;
+    }
+}
+else{
+    limparInformacoes();
+}
+};
